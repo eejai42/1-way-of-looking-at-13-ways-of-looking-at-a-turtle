a58d4bb24609f1c9fa2cfaf8d38eb975b114e6c4
eejai42 <start@anabstractlevel.com>
2017-11-10 17:53:29 -0600

    Created 1 tool (to establish pattern for the code replacement.   Next step will be to generate 15 tools.
    Updated the SSoT (for playing with the first tool).

--- 01-OOTurtle.fsx
+++ 01-OOTurtle.fsx
@@ -12,65 +12,43 @@ and the client talks to the turtle directly.
 
 ====================================== *)
 
-#load "Common.fsx"
+#load "OOTurtleBase.fsx"
+
+// #load "Common.fsx"
 
 open System
 open Common
 
+
+
 // ======================================
 // OO Turtle
 // ======================================
+// #load "OOTurtleLib.fsx"
 
 // see code in this file
-#load "OOTurtleLib.fsx"
+
+open OOTurtleBase
 
 // ======================================
 // OO Turtle Client
 // ======================================
 
+
 module OOTurtleClient = 
     open OOTurtleLib
+
 
     /// Function to log a message
     let log message =
         printfn "%s" message 
 
     let drawTriangle() = 
-        let turtle = Turtle(log)
-        turtle.Move 100.0 
-        turtle.Turn 120.0<Degrees>
-        turtle.Move 100.0 
-        turtle.Turn 120.0<Degrees>
-        turtle.Move 100.0
-        turtle.Turn 120.0<Degrees>
-        // back home at (0,0) with angle 0
+        OOTurtleBase.drawTriangle()
             
     let drawThreeLines() = 
-        let turtle = Turtle(log)
-        // draw black line 
-        turtle.PenDown()
-        turtle.SetColor Black
-        turtle.Move 100.0 
-        // move without drawing
-        turtle.PenUp()
-        turtle.Turn 90.0<Degrees>
-        turtle.Move 100.0 
-        turtle.Turn 90.0<Degrees>
-        // draw red line 
-        turtle.PenDown()
-        turtle.SetColor Red
-        turtle.Move 100.0
-        // move without drawing
-        turtle.PenUp()
-        turtle.Turn 90.0<Degrees>
-        turtle.Move 100.0 
-        turtle.Turn 90.0<Degrees>
-        // back home at (0,0) with angle 0
-        // draw diagonal blue line 
-        turtle.PenDown()
-        turtle.SetColor Blue
-        turtle.Turn 45.0<Degrees>
-        turtle.Move 100.0
+        OOTurtleBase.drawThreeLines()
+
 
     let drawPolygon n = 
         let angle = 180.0 - (360.0/float n) 
